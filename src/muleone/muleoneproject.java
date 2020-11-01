ecbbc9463166522a07bd2c85942367e54efbe502
Meronmk15 <mareyefeker15@gmail.com>
2020-11-01 17:40:41 -0500

    project one

---
+++ src/muleone/muleoneproject.java
@@ -0,0 +1,9 @@
+package muleone;
+
+public class muleoneproject {
+    public static void main(String[] args) {
+        System.out.println("Meron is smart");
+        System.out.println("Meron is funny");
+    }
+
+}
